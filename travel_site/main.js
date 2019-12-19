c1b42d91a96ea74c528ace4b936243eced543e47
GeorgiArgirov <mrargirov@gmail.com>
2019-12-19 22:27:58 +0200

    as

---
+++ travel_site/main.js
@@ -0,0 +1,10 @@
+var mainMenu = document.getElementById('mainMenu');
+
+function toggleMenu(){
+    if(mainMenu.style.display === 'block'){
+        mainMenu.style.display = 'none';
+    } else {
+        mainMenu.style.display = 'block';
+    }
+}
+
