b41525b502d97084dd3e3603cbff08fce4f6efa6
Georgi Argirov <mrargirov@gmail.com>
2020-01-22 17:53:00 +0200

    u

---
+++ react-app/src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max' , age: 28},
+      { name: 'George' , age: 30},
+      { name: 'Merry' , age: 25}
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  };
+
+ 
+
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked!');
+    //this.state.persons[0].name = 'Maximum';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28},
+        { name: 'George' , age: 30},
+        { name: 'Merry' , age: 30}
+      ]
+    } )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28},
+        { name: event.target.value,  age: 30},
+        { name: 'Merry' , age: 30}
+      ]
+    } )
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+
+
+  render () {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+        style={style}
+        onClick={this.togglePersonsHandler}>Switch Name</button>
+        { 
+          this.state.showPersons === true ?
+            <div>
+              <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age} />
+              <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Max!')}
+                changed={this.nameChangedHandler} >My Hobbies: Skiing</Person>
+              <Person 
+                name={this.state.persons[2].name} 
+                age={this.state.persons[2].age} />
+            </div> : null
+        }
+        
+      </div>
+    );
+
+  }
+
+}
+  
+export default App;
+
+
+
