4d9357ebbea51fffb6094aa0d44314d00d7ae124
Georgi Argirov <mrargirov@gmail.com>
2020-02-16 14:45:01 +0200

    src_test1

--- test1/main.js
+++ test1/main.js
@@ -1,11 +1,29 @@
-// console.log(alert('Hello !!!'));
 
-var mainMenu = document.getElementById('mainMenu');
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li a');
 
-function toggleMenu(){
-    if(mainMenu.style.display === 'block'){
-        mainMenu.style.display = 'none';
-    } else {
-        mainMenu.style.display = 'block';
-    }
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s `;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
 }
+navSlide();
+
+// console.log(alert('hello'));
+
+
+
+
+
+
